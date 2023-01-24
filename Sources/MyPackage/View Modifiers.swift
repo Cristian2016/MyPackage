e1ccc7bd36797010c46533cf8680a29ce1832dc0
Cristian <Cristian>
2023-01-24 21:00:23 +0200

    ok

--- Sources/MyPackage/View Modifiers.swift
+++ Sources/MyPackage/View Modifiers.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 @available(iOS 16.0, *)
-public struct ReadSize:ViewModifier {
+public struct SizeReader:ViewModifier {
     @Binding var size:CGSize
     public func body(content: Content) -> some View {
         content
@@ -22,9 +22,28 @@ public struct ReadSize:ViewModifier {
 }
 
 @available(iOS 16.0, *)
+public struct FrameReader:ViewModifier {
+    @Binding var frame:CGRect
+    public func body(content: Content) -> some View {
+        content
+            .background {
+                GeometryReader { geo -> Color in
+                    DispatchQueue.main.async { frame = geo.frame(in: .global) }
+                    return .clear
+                }
+            }
+    }
+}
+
+@available(iOS 16.0, *)
 public extension View {
     ///gets the size of the view the modifier is applied to
     func readSize(_ size:Binding<CGSize>) -> some View {
-        self.modifier(ReadSize(size: size))
+        self.modifier(SizeReader(size: size))
+    }
+    
+    ///gets the frame of the view the modifier is applied to
+    func readFrame(_ frame:Binding<CGRect>) -> some View {
+        self.modifier(FrameReader(frame: frame))
     }
 }
