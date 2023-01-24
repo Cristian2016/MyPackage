9a01154c299f8d044fb3ffa560190bf240c7599b
Cristian <Cristian>
2023-01-24 21:27:08 +0200

    okok

--- Sources/MyPackage/View Modifiers.swift
+++ Sources/MyPackage/View Modifiers.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 @available(iOS 16.0, *)
 public struct SizeReader:ViewModifier {
-    @Binding var size:CGSize
+    @Binding var size:CGSize?
     public func body(content: Content) -> some View {
         content
             .background {
@@ -23,7 +23,7 @@ public struct SizeReader:ViewModifier {
 
 @available(iOS 16.0, *)
 public struct FrameReader:ViewModifier {
-    @Binding var frame:CGRect
+    @Binding var frame:CGRect?
     public func body(content: Content) -> some View {
         content
             .background {
@@ -38,12 +38,12 @@ public struct FrameReader:ViewModifier {
 @available(iOS 16.0, *)
 public extension View {
     ///gets the size of the view the modifier is applied to
-    func readSize(_ size:Binding<CGSize>) -> some View {
+    func readSize(_ size:Binding<CGSize?>) -> some View {
         self.modifier(SizeReader(size: size))
     }
     
     ///gets the frame of the view the modifier is applied to
-    func readFrame(_ frame:Binding<CGRect>) -> some View {
+    func readFrame(_ frame:Binding<CGRect?>) -> some View {
         self.modifier(FrameReader(frame: frame))
     }
 }
